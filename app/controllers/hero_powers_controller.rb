a27bb2d1788c58dd00d0190fb29a03878eeaacdc
principalkelvo <principalkelvo@gmail.com>
2022-10-07 01:59:23 +0300

    only creates a hero strength measure

--- app/controllers/hero_powers_controller.rb
+++ app/controllers/hero_powers_controller.rb
@@ -1,2 +1,20 @@
 class HeroPowersController < ApplicationController
+    rescue_from ActiveRecord::RecordInvalid, with: :render_unprocessable_entity_response
+    def create
+        # create! exceptions will be handled by the rescue_from ActiveRecord::RecordInvalid code
+        hero_power = HeroPower.create!(hero_power_params)
+        render json: Hero.find(params[:hero_id]), status: :created
+        # render json: hero_power, status: :created
+    end
+
+
+  private
+
+  def hero_power_params
+    params.permit(:strength, :hero_id, :power_id)
+  end
+
+  def render_unprocessable_entity_response(invalid)
+    render json: { errors: invalid.record.errors }, status: :unprocessable_entity
+  end
 end
