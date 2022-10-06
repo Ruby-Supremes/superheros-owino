711be7dd3086eea8ca158c15c205f0c180922549
principalkelvo <principalkelvo@gmail.com>
2022-10-07 01:23:00 +0300

    returns json for all heros and each hero

--- app/controllers/heros_controller.rb
+++ app/controllers/heros_controller.rb
@@ -1,2 +1,23 @@
 class HerosController < ApplicationController
+    rescue_from ActiveRecord::RecordNotFound, with: :render_not_found_response
+
+    def index
+        render json: Hero.all, status: :ok
+    end
+
+    def show
+        hero = Hero.find(params[:id])
+        render json: hero, include: :powers, status: :ok
+    end
+
+    def destroy
+        hero = Hero.find(params[:id])
+        hero.destroy
+    end
+
+    private 
+    
+    def render_not_found_response
+        render json: { error: "Restaurant not found" }, status: :not_found
+      end
 end
