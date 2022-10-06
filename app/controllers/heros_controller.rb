ad51b1594a4deb4b04a37d9e727326aeb03293ac
principalkelvo <principalkelvo@gmail.com>
2022-10-07 02:01:06 +0300

    view all powers and get also each power you can update the description of a power

--- app/controllers/heros_controller.rb
+++ app/controllers/heros_controller.rb
@@ -16,8 +16,8 @@ class HerosController < ApplicationController
     end
 
     private 
-    
+
     def render_not_found_response
-        render json: { error: "Restaurant not found" }, status: :not_found
+        render json: { error: "Hero not found" }, status: :not_found
       end
 end
--- app/controllers/powers_controller.rb
+++ app/controllers/powers_controller.rb
@@ -1,2 +1,28 @@
 class PowersController < ApplicationController
+    rescue_from ActiveRecord::RecordNotFound, with: :render_not_found_response
+
+    def index
+        render json: Power.all
+    end
+
+    def show
+        power = Power.find(params[:id])
+        render json: power, status: :ok
+    end
+
+    def update
+        power = Power.find_by(id: params[:id])
+        power.update(power_params)
+        render json: power, status: :ok
+    end
+
+    private
+
+    def render_not_found_response
+        render json: { error: "Power not found" }, status: :not_found
+      end
+
+      def power_params
+        params.permit(:description)
+      end
 end
