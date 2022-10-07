38e9e2bba79718acc39431ae042c9721d54e4866
principalkelvo <principalkelvo@gmail.com>
2022-10-07 12:12:25 +0300

    and validations

--- app/models/hero_power.rb
+++ app/models/hero_power.rb
@@ -1,4 +1,10 @@
 class HeroPower < ApplicationRecord
+  STRENGTH = ['Strong', 'Weak', 'Average']
+
+  validates :strength, inclusion: {
+    in: STRENGTH,
+    message: "must be one of: #{STRENGTH.join(', ')}"
+  }
   belongs_to :hero
   belongs_to :power
 end
